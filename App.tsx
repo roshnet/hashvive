dfcea2d25699c912c376d1b0fb446204af1e9b25
Roshan Sharma <reprogram46@gmail.com>
2020-11-15 17:28:28 +0530

    Add two screens using drawer navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer } from '@react-navigation/native'
+import DemoScreen from './screens/Demo'
+import SettingsScreen from './screens/Settings'
+
+const Drawer = createDrawerNavigator()
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>This is the damn home screen</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Demo" component={DemoScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   )
 }
 
---
+++ screens/Settings.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Text>Settings Screen</Text>
+    </View>
+  )
+}

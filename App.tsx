3dbcdd0752b7954b3bbcaafbc13aa5fef0c53ff8
Roshan Sharma <reprogram46@gmail.com>
2020-11-16 04:34:22 +0530

    refactor: Define HomeScreen under screens directory

--- App.tsx
+++ App.tsx
@@ -3,17 +3,10 @@ import { StyleSheet, Text, View } from 'react-native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
 import DemoScreen from './screens/Demo'
+import HomeScreen from './screens/Home'
 import SettingsScreen from './screens/Settings'
 
 const Drawer = createDrawerNavigator()
-
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>This is the damn home screen</Text>
-    </View>
-  )
-}
 
 export default function App() {
   return (
@@ -26,12 +19,3 @@ export default function App() {
     </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})

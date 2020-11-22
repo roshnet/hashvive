a13800d17e586f65c9d368f063313d9081dee942
Roshan Sharma <reprogram46@gmail.com>
2020-11-22 18:03:59 +0530

    App.tsx: Create SQLite table schema on first run

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
+import * as SQLite from 'expo-sqlite'
 import React from 'react'
 import { Provider as PaperProvider } from 'react-native-paper'
 import DemoScreen from './screens/Demo'
 import HomeScreen from './screens/Home'
 import SetPasswordScreen from './screens/SetPassword'
 import SettingsScreen from './screens/Settings'
+
+const db = SQLite.openDatabase('app.db')
+
+db.transaction((tx) => {
+  tx.executeSql(
+    `
+    CREATE TABLE IF NOT EXISTS credentials (
+      site TEXT,
+      password TEXT
+    )`,
+    [],
+  )
+})
 
 const Drawer = createDrawerNavigator()
 

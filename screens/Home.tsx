5aab8eeb76ffda887ea2fb2bc5c35a389e288a7a
Roshan Sharma <reprogram46@gmail.com>
2020-11-23 02:56:47 +0530

    feat: Create credential entries and display as a list

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import * as SQLite from 'expo-sqlite'
+import React, { useEffect, useState } from 'react'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import Entry from '../components/Entry'
 import PasswordInput from '../components/PasswordInput'
 
+const db = SQLite.openDatabase('app.db')
+
 export default function HomeScreen() {
+  const [entries, setEntries] = useState<
+    Array<{ site: string; password: string }>
+  >([])
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(`SELECT * FROM credentials`, [], (t, { rows }) => {
+        let results: any = rows
+        setEntries(results._array)
+      })
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
       <PasswordInput />
+      <ScrollView style={styles.entries}>
+        {entries.map((entry) => {
+          return (
+            <Entry
+              key={Math.random()}
+              site={entry.site}
+              password={entry.password}
+            />
+          )
+        })}
+      </ScrollView>
     </View>
   )
 }
@@ -15,5 +43,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#224',
     alignItems: 'center',
+  },
+  entries: {
+    width: '100%',
   },
 })
